c6c66157d339a7d9c187caaf2c457ce01b118980
kashishgupta2911 <guptakashish2911@gmail.com>
2025-03-01 13:04:56 -0700

    Profile and Card History

---
+++ code/app/src/main/java/com/example/arcmobile/Transaction.java
@@ -0,0 +1,31 @@
+package com.example.arcmobile;
+
+public class Transaction {
+    private String date;
+    private String time;
+    private String amount;
+    private String location;
+
+    public Transaction(String date, String time, String amount, String location) {
+        this.date = date;
+        this.time = time;
+        this.amount = amount;
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
